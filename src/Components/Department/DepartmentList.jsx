c52faeb3ae3290acec79021adf9ce2effe8390b2
sarthak-2233 <sarthak.sharma33333@gmail.com>
2025-03-25 00:15:52 +0530

    project half

--- src/Components/Department/DepartmentList.jsx
+++ src/Components/Department/DepartmentList.jsx
@@ -9,7 +9,7 @@ function DepartmentList() {
       </div>
 
       <div className='flex justify-between items-center'>
-        <input type='text' placeholder='Search By Department Name' className='w-[240px] p-2'></input>
+        <input type='text' placeholder='Search By Department Name' className='w-[240px] p-2 border border-slate-400 rounded-md focus:outline-none focus:border-indigo-500 transition duration-300'></input>
         <Link to="/admin-dashboard/add-department" className='px-4 py-1 bg-slate-700 hover:cursor-pointer hover:bg-slate-600 rounded text-white'>Add New Department</Link>
         </div>     
 
--- src/Components/Leave/Leave.jsx
+++ src/Components/Leave/Leave.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Leave() {
   return (
     <div>
-      This is leave
+      Updating Ui ... Loading soon
     </div>
   )
 }
